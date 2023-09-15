68c7d418b0f3be62a3e41fc7151fc055a8bb5c92
platformer500 <platformer500@gmail.com>
2023-09-15 09:28:11 -0400

    Added journal functionality

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('feature1')
+# Program Name: Murcury Terminal
+# Purpose: Creating journals and handeling tasks
+# Developer: Roderick Azevedo
+# Date: 9/15/2023
+
+import os
+from datetime import datetime
+
+# Create directory
+journaldir = "Journal Entries"
+os.makedirs(journaldir, exist_ok=True)
+
+# Get current date
+now = datetime.now()
+entryDate = now.strftime("%Y-%H-%M")
+
+# Create File
+entryFileName = f"{journaldir}/{entryDate}.txt"
+with open(entryFileName, "w") as entryFile:
+    entryText = input("Enter your journal entry: ")
+    entryFile.write(entryText)
+
+# print results
+print(f"Journal entry saved to {entryFileName}")
