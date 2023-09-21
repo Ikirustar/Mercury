e792c7d60ca5b6ca70e2debb6ffaf2075d532e68
platformer500 <=>
2023-09-21 10:32:33 -0400

    Leaning up master

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 # Purpose: Creating journals and handeling tasks
 # Developer: Roderick Azevedo
 # Date: 9/15/2023
+"""
+ai prompt 
 
-# ai prompt: Act as my mentor. I want to create a directory named checklist and allow users to create their own directory in it. 
-# The program needs to allow users to create their own folders within those directories as checklists. 
-
+Act as my mentory and help me develop a python project. (I am familier with the syntax of Python and have expreience with a couple of libraries. 
+But not an expert and I do not know frameworks and project structure). 
+This project allows users to create journals and write checklists in a GUI. It is also a note taking
+application that has formatting features such as different heading and intellisense. GPT4 will give suggestions on notes and be used to keep track of information any
+users forget. This information is stored in a txt file. {}. 
+"""
 import os
 import sys
 from datetime import datetime
