f3c8dee54c640c5ce72dcbac1e0c8beda80e8b2d
platformer500 <platformer500@gmail.com>
2023-12-08 13:31:34 -0500

    Added menu bar
    -Read Me created
    -Menu bar file options added
    -Commands need to be created
    -Other menu cascades need to be added

--- GUI.py
+++ GUI.py
@@ -1,19 +1,66 @@
+
+#* Modules
 import customtkinter
+import tkinter 
+from CTkMenuBar import *
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+
+# window
 root = customtkinter.CTk()
-root.geometry("500x350")
+root.geometry("600x400")
+root.title("Mercury")
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-my_font = customtkinter.CTkFont(family="Roboto", size=24)
-my_font.configure(family="Name")
+button = customtkinter.CTkButton(root, text="CTkButton")
 
-label = customtkinter.CTkLabel(master=frame, text="MURCURY", font=my_font)
-label.pack(pady=12, padx=1)
+#* widgets 
+dropdown = CustomDropdownMenu(widget=button)
+dropdown.add_option(option="value") 
+dropdown.add_separator() 
+
+menu = CTkMenuBar(master=root)
+m1 = menu.add_cascade("File")
+
+file_dropdown = CustomDropdownMenu(widget=m1)
+file_dropdown.add_option(option="New File")
+file_dropdown.add_option(option="Open")
+file_dropdown.add_option(option="Save")
+file_dropdown.add_option(option="Save As")
+file_dropdown.add_separator()
+file_dropdown.add_option(option="Quit")
+
+menu.add_cascade("Edit")
+menu.add_cascade("Settings")
+menu.add_cascade("About")
+
+#* widgets
+#! Commands removed for now 
+#! These include open_file, theme_selection, help_us
+
+# menu_bar = tkinter.Menu(root, background='blue', fg='black')
+# m1 = tkinter.Menu(menu_bar, tearoff=0)
+# m1.add_command(label="Open File")
+# m1.add_separator()
+# m1.add_command(label="Save File")
+# root.config(menu=menu_bar)
+
+# menu_bar.add_cascade(label="File",menu=m1)
+# m2 = tkinter.Menu(menu_bar, tearoff=0)
+# m2.add_command(label="Light theme")
+# m2.add_command(label="Dark theme")
+# m2.add_separator()
+# m2.add_command(label="System theme")
+# root.config(menu=menu_bar)
+# menu_bar.add_cascade(label="Setting",menu=m2)
+
+# m3 = tkinter.Menu(menu_bar, tearoff=0)
+# m3.add_command(label="help!")
+
+# root.config(menu=menu_bar)
+# menu_bar.add_cascade(label="Help",menu=m3)
 
 # textbox = customtkinter.CTkTextbox()
 
