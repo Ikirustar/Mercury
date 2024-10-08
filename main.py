fb77297ec9f4d57c33ccc7f9c19f7c8445d4acc9
Ikirustar <ikirustar@gmail.com>
2024-10-08 02:02:21 -0300

    Temporary path added

--- main.py
+++ main.py
@@ -73,8 +73,9 @@ while True:
     print("2. Checklist")
     print("3. Quit")
     choice = input('Select an option: ')
-    
-    os.chdir("C:\\Users\\platf\\OneDrive\\Desktop\\Mercury")
+
+    # ! Temporary path
+    os.chdir("C:\\Users\\platf\\OneDrive\\Desktop\\Projects\\Mercury")
 
     if choice == "1":
         create_journal()
